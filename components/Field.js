834213e5b165bc3b515e64cbf897781eaa3ee8cf
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-04-02 20:49:38 +0000

    Automatically backed up by Learn

--- components/Field.js
+++ components/Field.js
@@ -1,12 +1,11 @@
 import React from 'react';
 
-export default class Field extends React.Component {
-  render () {
+const Field = (props) => {
     return (
       <button className="field" onClick={event => this.props.onClick(this.props.index, event) } >
         {this.props.player}
       </button>
     );
-  }
 }
 //this is a field
+export default Field
